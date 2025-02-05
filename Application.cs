9c405e64ac8b3a7d40ca78afc8fa5201c34407e9
Ricardo Garcia Braga <ricardobriangarcia@gmail.com>
2025-02-05 15:59:04 -0300

    Ready to Code
    All references and setup ready
    Lets code the application class

---
+++ Application.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+
+namespace ProjetaHDR
+{
+    public class Application : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+
+        }
+    }
+}
