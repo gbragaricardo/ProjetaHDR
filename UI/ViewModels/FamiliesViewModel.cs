fc093b05ae5af673aa935c80c612ae0a6b3c246b
Ricardo Garcia Braga <ricardobriangarcia@gmail.com>
2025-04-03 09:49:38 -0300

    Apresentado para arq

--- UI/ViewModels/FamiliesViewModel.cs
+++ UI/ViewModels/FamiliesViewModel.cs
@@ -115,39 +115,4 @@ namespace ProjetaHDR.UI.ViewModels
         }
 
     }
-
-
-
-
-
-
-
-
-    //internal class FamiliesViewModel : ObservableObject
-    //{
-    //    private string _familiesFolder = @"C:\Families"; // Defina o caminho das famílias
-
-    //    public ObservableCollection<string> Families { get; set; }
-
-    //    public ICommand RefreshCommand { get; }
-
-    //    public FamiliesViewModel()
-    //    {
-    //        Families = new ObservableCollection<string>();
-    //        RefreshCommand = new RelayCommand(LoadFamilies);
-    //        LoadFamilies();
-    //    }
-
-    //    private void LoadFamilies(object parameter = null)
-    //    {
-    //        Families.Clear();
-    //        if (Directory.Exists(_familiesFolder))
-    //        {
-    //            foreach (var file in Directory.GetFiles(_familiesFolder, "*.rfa"))
-    //            {
-    //                Families.Add(Path.GetFileName(file));
-    //            }
-    //        }
-    //    }
-    //}
 }
